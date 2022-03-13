f966ecd1e4d2b6099d6b123f5d75ed16e8a1acd6
Shreyas Ashtamkar <work.shreyas.ashtamkar@gmail.com>
2022-03-13 07:55:53 +0530

    Removed Eleavte Code; Remove Logs from github;

--- start.ps1
+++ start.ps1
@@ -1,5 +1,2 @@
-set "params=%*"
-cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-
 Write-Output "Auto Power Mode Starting"
 runas /user:Administrator .\nssm.exe start "AUTO_POWER_MODE"
--- uninstall.ps1
+++ uninstall.ps1
@@ -1,5 +1,2 @@
-set "params=%*"
-cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-
 .\nssm.exe stop "AUTO_POWER_MODE"
 .\nssm.exe remove "AUTO_POWER_MODE" confirm
